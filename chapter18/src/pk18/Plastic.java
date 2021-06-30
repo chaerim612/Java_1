8f5310f9dc19b9ef9e8cbf4c16eaaa52ef19afbd
chaerim612 <86398466+chaerim612@users.noreply.github.com>
2021-06-30 18:13:03 +0900

    Delete chapter18 directory

--- chapter18/src/pk18/Plastic.java
+++
@@ -1,8 +0,0 @@
-package pk18;
-
-public class Plastic {
-	public String toString() {
-		return "재료는 Plastic입니다. ";
-	}
-
-}
--- chapter18/src/pk18/Powder.java
+++
@@ -1,7 +0,0 @@
-package pk18;
-
-public class Powder {
-	public String toString() {
-		return "재료는 Powder입니다. ";
-	}
-}
